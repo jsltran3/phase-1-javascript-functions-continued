365cc93cf8bb35ac3ed0db0ee3b2c2fb8762e093
Joseph <jsltran@gmail.com>
2021-12-17 01:49:48 -0800

    commit tree 959e7504a74292349673ba7dd58f7d844d150b9b

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// code your solution here
+// function saturdayFun(activity = 'roller-skate') {
+//    console.log(`This Saturday, I want to ${activity}!`);
+// }
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(boring = "go to the office") {
+    return `This Monday, I will ${boring}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function innerFunction(adj = "special") {
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
+
+
+
+
+
+
+
+// function wrapAdjective(param = "*") {
+//     const part1 = "You are"
+//     return function innerFunction(param2="special") {
+// return `${part1} ${param}${param2}${param}!`
+//     };
+// }
+
+
+//need someone to expalin to me why it can't all be in one string. what are the limitations of the functions that require us to dynamically take all these arguemnts 
+
+//or teach me how to read this
